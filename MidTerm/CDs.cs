2fb557966234008bf50933f64e0b54fd3c2918c3
duncaannn <69321122+duncaannn@users.noreply.github.com>
2020-10-29 15:53:31 -0400

    test

--- MidTerm/CDs.cs
+++ MidTerm/CDs.cs
@@ -4,7 +4,11 @@ using System.Text;
 
 namespace MidTerm
 {
+<<<<<<< Updated upstream:MidTerm/CDs.cs
     class CDs
+=======
+    class Cassettes :Albums
+>>>>>>> Stashed changes:MidTerm/MidTerm/Cassettes.cs
     {
     }
 }

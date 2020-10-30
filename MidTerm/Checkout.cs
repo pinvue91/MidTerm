c7ac778886eb9a969f877a0d8680bc25d3521805
nickahickman <nickahickman@gmail.com>
2020-10-30 07:57:53 -0400

    stubbing out checkout class

---
+++ MidTerm/Checkout.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MidTerm
+{
+    class Checkout
+    {
+        public List<Albums> albums = new List<Albums>() 
+        { 
+            
+        };
+        public List<Albums> ShoppingCart { get; set; }
+        public static void Start()
+        {
+            MyLibs.ConsoleLibrary.DrawTitle("Welcome to GC Records");
+
+            while (true)
+            {
+
+
+                if (!MyLibs.UserInputLibrary.UserWantsToContinue("Would you like anything else? We have lots of great stuff", "Invalid entry"))
+                {
+                    Console.WriteLine("Thanks for shopping with us!");
+                }
+            }
+        }
+
+        public static void PrintCatalogue()
+        { 
+        
+        }
+
+        public static void AddItemToShoppingCart()
+        { 
+        
+        }
+
+        public static void PrintShoppingCart()
+        {
+
+        }
+
+        public static void RemoveItemFromShoppingCart()
+        { 
+        
+        }
+
+        public static void PrintReceipt()
+        { 
+        
+        }
+    }
+}

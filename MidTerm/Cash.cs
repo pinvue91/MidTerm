837bfd90f8c7c8e2ac5ff937c494cf131012ff5f
pinvue91 <pin.vue@hotmail.com>
2020-11-01 15:52:22 -0500

    re-uploading payment classes

---
+++ MidTerm/Cash.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MidTerm
+{
+    class Cash : Payment
+    {
+        //no unique properites
+
+
+        //constructor
+        public Cash(int AlbumPrice, int Quantity) : base(AlbumPrice, Quantity)
+        {
+
+        }
+
+
+        //override Methods
+
+
+
+    }
+}
---
+++ MidTerm/CreditCard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MidTerm
+{
+    class CreditCard : Payment
+    {
+        //Credit Card Properties
+        public int CreditCardNumber { get; set; }
+        public string ExpirationDate { get; set; }
+        public int CW { get; set; }
+
+        //constructor
+        public CreditCard(int AlbumPrice, int Quantity, int CreditCardNumber, string ExpirationDate, int CW) : base(AlbumPrice, Quantity)
+        {
+            this.CreditCardNumber = CreditCardNumber;
+            this.ExpirationDate = ExpirationDate;
+            this.CW = CW;
+
+        }
+
+        //method overrides
+
+
+    }
+}
